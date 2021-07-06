f1079b53bb694b208512f7eb4bc4434dc9ad19a3
Mr Markutan <mrmarkutan@gmail.com>
2021-07-06 20:30:03 +0300

    Fixed Broken Links. Closes #45

--- sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -4,7 +4,7 @@ import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-@RequestMapping("/vets")
+//@RequestMapping("/vets")
 @Controller
 public class VetController {
 
@@ -14,7 +14,7 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"/index.html", "/index", "/", ""})
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
     public String listVets(Model model){
 
         model.addAttribute("vets", vetService.findAll());
